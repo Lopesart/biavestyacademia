a4c364a759dbd59a1f191f002878a1969ddb8173
Lopes <128938685+Lopesart@users.noreply.github.com>
2023-06-22 00:25:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# biavestyacademia
+ biapersonal
---
+++ scrip.js
@@ -0,0 +1,51 @@
+function mostrarLinks() {
+    var linksDiv = document.getElementById("links");
+
+    if (linksDiv.style.display === "none") {
+      linksDiv.style.display = "block";
+    } else {
+      linksDiv.style.display = "none";
+    }
+  }
+
+
+
+
+
+function scrollToDiv1() {
+    var div = document.getElementById("p1");
+    div.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  
+  function scrollToDiv2() {
+    var div = document.getElementById("p2");
+    div.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  
+  function scrollToDiv3() {
+    var div = document.getElementById("p3");
+    div.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  
+  function scrollToDiv4() {
+    var div = document.getElementById("p4");
+    div.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  function scrollToDiv5() {
+    var div = document.getElementById("p5");
+    div.scrollIntoView({ behavior: 'smooth' });
+  };
+  
+  function scrollToDiv6() {
+    var div = document.getElementById("p6");
+    div.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  function scrollToDiv7() {
+    var div = document.getElementById("p7");
+    div.scrollIntoView({ behavior: 'smooth' });
+  };
